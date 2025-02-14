d9c4baed9780ee5c12e4b351aed1874b32d4e608
Kevin Chen <kchen@lcs.on.ca>
2025-02-14 01:21:56 -0500

    Initial Commit

---
+++ Stepper/StepperApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperApp.swift
+//  Stepper
+//
+//  Created by Haowen Chen on 2025-02-14.
+//
+
+import SwiftUI
+
+@main
+struct StepperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
